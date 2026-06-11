609257a5cc5f903abdb223a8876e457c26d5202e
deshmukhbibhishan4-max <deshmukhbibhishan4@gmail.com>
2026-06-11 10:55:51 +0530

    Production API URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
 
-  const API = "http://localhost:5000/students";
+  const API = "https://mern-crud2-quv2.onrender.com/students";
 
   const [students, setStudents] = useState([]);
   const [name, setName] = useState("");
